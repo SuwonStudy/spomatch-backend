b9656e0bde5fc9a14952d7601a8d21e7c73b0880
Seongbin Kim <seongbin9786@gmail.com>
2018-11-04 07:39:32 +0900

    init

---
+++ src/main/java/com/spomatch/SpoMatchApplication.java
@@ -0,0 +1,12 @@
+package com.spomatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoMatchApplication.class, args);
+	}
+}
